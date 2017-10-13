730e3df3a343d20b0673ffd55209019a19623edf
jechedo <jechedo@163.com>
2017-10-13 18:04:02 +0800

    设计规则查询器

--- project/aptrules/src/main/java/cn/skyeye/aptrules/ioc2rules/Ruler.java
+++ project/aptrules/src/main/java/cn/skyeye/aptrules/ioc2rules/Ruler.java
@@ -4,9 +4,15 @@ import cn.skyeye.aptrules.ARConf;
 import cn.skyeye.aptrules.ARContext;
 import org.apache.log4j.Logger;
 
+import java.util.Collection;
+
 /**
  * Description:
  *     rules表的相关操作
+ *     目前其职责考虑为：
+ *         1. 将Ioc2RuleHandler推过来的是rule存入到sqlite(严格意义上算是覆盖)
+ *         2. 将Ioc2RuleHandler推过来的是rule缓存再内存（本地内存或推入redis），供实时告警查询
+ *         3. 在启动时主动同步sqlite中的rule
  * @author LiXiaoCong
  * @version 2017/10/13 17:09
  */
@@ -19,13 +25,27 @@ public class Ruler {
 
     public Ruler(){
         this.arConf = ARContext.get().getArConf();
+        //同步sqlite中的rule
+        listRules();
     }
 
     public void listRules(){
 
     }
 
-    public void storeRules(){
+    /**
+     *  覆盖sqlite中原有的rule信息
+     */
+    public void overrideRules(){
 
+    }
+
+    /**
+     *  由日志数据中的告警字段合成 ruleKey， 然后再规则中查找是否存在对应的告警规则。
+     * @param ruleKey
+     * @return   null  or  Collection<Rule>
+     */
+    public Collection<Rule> findRules(String ruleKey){
+        return null;
     }
 }
