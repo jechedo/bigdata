0f05db230bf9cc7a0f7a98e59d32f8ef735547fb
jechedo <jechedo@163.com>
2017-12-04 23:28:01 +0800

    增加tcp错误记录

--- project/norths_es2/src/main/java/cn/skyeye/norths/services/syslog/Log4jBackLogHandler.java
+++ project/norths_es2/src/main/java/cn/skyeye/norths/services/syslog/Log4jBackLogHandler.java
@@ -12,6 +12,7 @@ import org.productivity.java.syslog4j.impl.backlog.log4j.Log4jSyslogBackLogHandl
  * @version 2017/12/4 23:11
  */
 public class Log4jBackLogHandler extends Log4jSyslogBackLogHandler {
+
     public Log4jBackLogHandler(Logger logger) throws SyslogRuntimeException {
         super(logger);
     }
@@ -21,7 +22,8 @@ public class Log4jBackLogHandler extends Log4jSyslogBackLogHandler {
         String var5 = var3 != null ? var3 : "UNKNOWN";
         String var6 = var4 != null ? var4 : "UNKNOWN";
         if (this.appendReason) {
-            var5 = var5 + "\n  " + var6;
+            var5 = String.format("%s \n %s[host = %s, port = %s, protocol = %s]",
+                    var4, var6, var1.getConfig().getHost(), var1.getConfig().getPort(), var1.getProtocol());
         }
 
         return var5;
